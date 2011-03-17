cf79c033d5b704dc63159454d8e9c57ce0086065
huangkaiguo <lennyapple@gmail.com>
2011-03-17 15:13:58 +0800

    eOrder: Change welcome interface
    1. Modify WelcomeActivity
    2. Add support for landscape
    
    FIX=Welcome(UI)

--- src/com/androidodc/WelcomeActivity.java
+++ src/com/androidodc/WelcomeActivity.java
@@ -2,12 +2,56 @@ package com.androidodc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class WelcomeActivity extends Activity {
+
+    private TextView welcomeView = null;
+    private Button enterButton = null;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
+
+        welcomeView = (TextView) findViewById(R.id.welcome);
+
+        String welcome = getResources().getString(R.string.welcome,
+                getHotelName());
+        welcomeView.setText(welcome);
+
+        enterButton = (Button) findViewById(R.id.enter);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                /*
+                 * The following code will be used, when TableAcitivity is
+                 * created.
+                 *
+                 * startActivity(new Intent(WelcomeActivity.this,
+                 * TablesActivity.class));
+                 */
+                finish();
+            }
+        });
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    private String getHotelName() {
+        // FIXME: need merge
+        return "our Restaurant";
+    }
+
 }
